9de82358bd0c8b8bf1be2f297977e6d5c179b6ba
Brady Blair <bblair321@yahoo.comm>
2025-03-21 08:06:53 -0700

    Created a new page for the tarkov installer, and styling for it.

--- js/main.js
+++ js/main.js
@@ -11,10 +11,16 @@ for (const square of squares) {
     // Get the background image URL from the data-bg attribute
     const bgImage = square.getAttribute('data-bg');
     const bgSize = square.getAttribute('data-size'); // Get the background-size setting
-    
+    const url = square.getAttribute('data-url'); // Get the URL from the data-url attribute
+
     // Set the background image dynamically
     square.style.backgroundImage = `url(${bgImage})`;
-    
-    // Set the background-size dynamically
     square.style.backgroundSize = bgSize;
+
+    // Make the square clickable
+    square.addEventListener('click', () => {
+        if (url) {
+            window.location.href = url; // Redirect to the specified URL
+        }
+    });
 }
