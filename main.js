6460c8ea1b90a4f1df0b5114e1f327cb74fceeb5
Brady Blair <bblair321@yahoo.comm>
2025-03-20 18:24:51 -0700

    created a flexbox design for the squares and javascript that will handle each background image separately and links

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 // JavaScript to make the square interactive
 
 // Get the square element
-const square = document.getElementById('square');
+const squares = document.querySelectorAll('.square');
 
 // Add event listener for click to toggle the "active" class
 const greet = (name) => `Hello, ${name}!`;
+
+// Add event listener for each square using for...of loop
+for (const square of squares) {
+    // Get the background image URL from the data-bg attribute
+    const bgImage = square.getAttribute('data-bg');
+    const bgSize = square.getAttribute('data-size'); // Get the background-size setting
+    
+    // Set the background image dynamically
+    square.style.backgroundImage = `url(${bgImage})`;
+    
+    // Set the background-size dynamically
+    square.style.backgroundSize = bgSize;
+}
