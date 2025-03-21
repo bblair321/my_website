26c459b642f58485cc38ad723287eba847d55311
Brady Blair <bblair321@yahoo.comm>
2025-03-20 17:35:27 -0700

    added a reactive square and a bunch of formatting work

---
+++ main.js
@@ -0,0 +1,7 @@
+// JavaScript to make the square interactive
+
+// Get the square element
+const square = document.getElementById('square');
+
+// Add event listener for click to toggle the "active" class
+const greet = (name) => `Hello, ${name}!`;
